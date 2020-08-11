6146359e00c274a514e60ea45ca1d1ed5e2a8d6b
i-whammy <yas.iwami@gmail.com>
2020-08-11 20:50:00 +0900

    added global header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <global-header></global-header>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import GlobalHeader from './components/GlobalHeader.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    GlobalHeader
   }
 }
 </script>
@@ -23,6 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="global-header">My social</header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'GlobalHeader',
+})
+</script>
+
+<style scoped>
+.global-header {
+    height: 40px;
+    padding: 8px 0;
+}
+</style>
