0a8f5d9aa0a0440ef938a4087d9fdd7ae3e53334
i-whammy <yas.iwami@gmail.com>
2020-09-01 23:59:44 +0900

    first articles

---
+++ src/components/Articles.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class='articles'>
+        <div v-for='article in articles' v-bind:key='article.id'>{{article.title}}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'Articles',
+    data: function() {
+        return { articles: [
+            { title: 'Hello world!', id: 1 },
+            { title: 'Hello world 2nd!', id: 2 },
+            { title: 'Hello world 3rd!', id: 3 },
+        ]}
+    }
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Articles from './components/Articles.vue'
 import SignInPage from './components/SignInPage.vue'
 import SignUpPage from './components/SignUpPage.vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
 const routes = [
-  { path: '/', component: HelloWorld },
+  { path: '/', component: Articles },
   { path: '/signin', component: SignInPage },
   { path: '/signup', component: SignUpPage }
 ]
