ed06dc376f28da588f2ce67a61ed2005ed86e8d3
i-whammy <yas.iwami@gmail.com>
2020-09-02 23:03:04 +0900

    added userid and fix style

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,6 +1,11 @@
 <template>
     <div class='articles'>
-        <div v-for='article in articles' v-bind:key='article.id'>{{article.title}}</div>
+        <div v-for='article in articles' v-bind:key='article.id'>
+            <div class='article'>
+                <div class='article-user'>{{article.userId}}</div>
+                <div class='article-title'>{{article.title}}</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,10 +15,26 @@ export default Vue.extend({
     name: 'Articles',
     data: function() {
         return { articles: [
-            { title: 'Hello world!', id: 1 },
-            { title: 'Hello world 2nd!', id: 2 },
-            { title: 'Hello world 3rd!', id: 3 },
+            { title: 'Hello world!', id: 1, userId: 'aaaa' },
+            { title: 'Hello world 2nd!', id: 2, userId: 'eeee' },
+            { title: 'Hello world 3rd!', id: 3, userId: 'ccccc' },
         ]}
     }
 })
 </script>
+
+<style lang="scss" scoped>
+.articles {
+}
+
+.article {
+    border-top: solid 1px;
+    height: 160px;
+    padding: 16px;
+}
+
+.article-title {
+    font-size: 24px;
+    margin: 16px 0;
+}
+</style>
